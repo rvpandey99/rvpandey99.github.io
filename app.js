32d55ea933acab5593de752d491b299688f0a18f
Ravi Prakash Pandey <rvpandey99@gmail.com>
2020-05-01 18:22:03 +0530

    added transitions

---
+++ app.js
@@ -0,0 +1,23 @@
+let hamberger = document.querySelector('#hamberger-menu')
+hamberger.addEventListener('click', function () {
+    hamberger.style.display = 'none';
+    let nav = document.querySelector('header nav'); 
+    nav.style.display = 'flex';
+    setTimeout(()=>{
+        nav.classList.add('horizTranslate');
+    },100);
+    setTimeout(()=>{
+        document.querySelector('#hamberger-x').style.display = 'inline-block';
+    },400);
+} );
+
+let hambergerX = document.querySelector('#hamberger-x')
+hambergerX.addEventListener('click', function () {
+    hambergerX.style.display = 'none';
+    let nav = document.querySelector('header nav');
+    nav.classList.remove('horizTranslate');
+    setTimeout(()=>{
+        nav.style.display = 'none';
+        document.querySelector('#hamberger-menu').style.display = 'inline-block';
+    },400);
+} );
